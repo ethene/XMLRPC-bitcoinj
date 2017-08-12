01aa048a620a4ac4f83afa1d598242a3cfa31d0a
strky <shroomshroom@gmail.com>
2017-08-12 16:29:57 +0700

    First commit of working bitcoinj daemon and mercury trading bot

--- mercury-telegram-bot.py
+++ mercury-telegram-bot.py
@@ -8,9 +8,8 @@ import coloredlogs
 import matplotlib as mpl
 import pandas as pd
 from sqlalchemy import (create_engine, Table, Column, Integer,
-                        String, MetaData)
+                        String, Boolean, MetaData)
 from sqlalchemy.sql import select
-from telegram import ReplyKeyboardMarkup, KeyboardButton
 from telegram.error import (TelegramError)
 from telegram.ext import CommandHandler
 from telegram.ext import Updater
@@ -35,29 +34,18 @@ def error_callback(bot, update, error):
 
 XMLRPCServer = xmlrpc.client.ServerProxy('http://localhost:8000')
 
-useraccounts_table = 'useraccounts'
-admin_table = 'adminusers'
+useraccounts_table = 'telegram_useraccounts'
 balance_table = 'mercury_balance'
+balance_diff_table = 'avg_balance_difference'
 pic_folder = './pictures'
 pic_1_filename = 'balance.png'
 pic_2_filename = 'cumulative.png'
 
 level = logging.DEBUG
-
 script_name = 'telegram.bot'
 
 db_engine = create_engine(MYSQL_CONNECTION, echo=False)
 metadata = MetaData(db_engine)
-
-if not db_engine.dialect.has_table(db_engine, admin_table):
-    logger.debug("admin accounts table does not exist")
-    # Create a table with the appropriate Columns
-    Table(admin_table, metadata,
-          Column('ID', Integer, primary_key=True, nullable=False),
-          Column('firstname', String(255)), Column('lastname', String(255)),
-          Column('username', String(255)))
-    # Implement the creation
-    metadata.create_all()
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(module)s - %(message)s', level=level)
 logger = logging.getLogger(script_name)
@@ -65,17 +53,22 @@ log_filename = './log/' + script_name + '.log'
 log_handler = SizedTimedRotatingFileHandler(log_filename, maxBytes=0, backupCount=5, when='D',
                                             interval=1)  # encoding='bz2',  # uncomment for bz2 compression)
 logger.addHandler(log_handler)
+coloredlogs.install(level=level)
+
+if not db_engine.dialect.has_table(db_engine, useraccounts_table):
+    logger.debug("user accounts table does not exist")
+    # Create a table with the appropriate Columns
+    Table(useraccounts_table, metadata,
+          Column('ID', Integer, primary_key=True, nullable=False),
+          Column('firstname', String(255)), Column('lastname', String(255)),
+          Column('username', String(255)), Column('isadmin', Boolean(), default=False), Column('address', String(40)))
+    # Implement the creation
+    metadata.create_all()
 
 updater = Updater(token=TELEGRAM_BOT_TOKEN)
 dispatcher = updater.dispatcher
-formatter = logging.Formatter(fmt='%(asctime)s - %(levelname)s - %(module)s - %(message)s')
-# log_handler = logging.StreamHandler()
-# log_handler.setFormatter(formatter)
-# logger.addHandler(log_handler)
-# logger.setLevel(level)
-coloredlogs.install(level=level)
 
-
+'''
 def userlist(bot, update):
     userfrom = update.effective_user
     username = userfrom.username
@@ -105,11 +98,12 @@ def userlist(bot, update):
             bot.send_message(chat_id=update.message.chat_id,
                              text="User / InValue / OutValue\n" + message)
 
+'''
 
 def start(bot, update):
     with db_engine.connect() as con:
-        adminaccounts = Table(admin_table, metadata, autoload=True)
-        stm = select([adminaccounts])
+        useraccounts = Table(useraccounts_table, metadata, autoload=True)
+        stm = select([useraccounts])
         rs = con.execute(stm)
         response = rs.fetchall()
         userfrom = update.effective_user
@@ -119,11 +113,15 @@ def start(bot, update):
         lastname = userfrom.last_name
         username = userfrom.username
         if len(response) == 0:
-            logger.debug("admins not found in db, creating new admin %s" % userfrom)
-            ins = adminaccounts.insert().values(ID=userID, firstname=firstname, lastname=lastname, username=username)
+            logger.debug("user not found in db, creating new admin %s" % userfrom)
+            ins = useraccounts.insert().values(ID=userID, firstname=firstname, lastname=lastname, username=username,
+                                               isadmin=False)
             con.execute(ins)
+            bot.send_message(chat_id=update.message.chat_id,
+                             text="Hello, %s!\nYour new account has just created" % (username))
             return
 
+        '''
         stm = select([adminaccounts]).where(adminaccounts.c.ID == userID)
         rs = con.execute(stm)
         response = rs.fetchall()
@@ -135,6 +133,8 @@ def start(bot, update):
             bot.send_message(chat_id=update.message.chat_id,
                              text="Hello, %s!\nWelcome back to use the bot" % (username),
                              reply_markup=ReplyKeyboardMarkup(keyboard=[[KeyboardButton(text="/statistics")]]))
+
+        '''
 
 
 def stats(bot, update):
@@ -169,12 +169,13 @@ def plot_graph(df, name, label):
     plt.plot(df)
     plt.savefig(pic_folder + '/' + name)
 
+
 start_handler = CommandHandler('start', start)
-stats_handler = CommandHandler('statistics', stats)
+# stats_handler = CommandHandler('statistics', stats)
 # userlist_handler = CommandHandler('userlist', userlist)
 # dispatcher.add_handler(userlist_handler)
 dispatcher.add_handler(start_handler)
-dispatcher.add_handler(stats_handler)
+#dispatcher.add_handler(stats_handler)
 
 dispatcher.add_error_handler(error_callback)
 updater.start_polling()
