2a3fe30417ec60fbdfa5d92012226920befa6f0a
strky <shroomshroom@gmail.com>
2017-08-30 00:19:30 +1000

    First commit of working bitcoinj daemon and mercury trading bot

--- XMLRPC-bitcoinj.py
+++ XMLRPC-bitcoinj.py
@@ -142,7 +142,7 @@ class RPCFunctions:
         legal = invalue - amount > org.bitcoinj.core.Transaction.REFERENCE_DEFAULT_MIN_TX_FEE.getValue()
         logger.debug("invalue: %d, to_send: %d, legal: %s " % (invalue, amount, legal))
         if legal:
-            c = org.bitcoinj.core.Coin().valueOf(amount)
+            c = org.bitcoinj.core.Coin.valueOf(amount)
             logger.debug(c)
         pg = self.kit.peerGroup()
         # sr = self.kit.wallet().sendCoins(pg, address, Coin(amount).subtract())
