613f48d00b8d2f8de038a7b12951e62dcd059af2
strky <shroomshroom@gmail.com>
2017-09-03 09:27:15 +1000

    First commit of working bitcoinj daemon and mercury trading bot

--- mercury-telegram-bot.py
+++ mercury-telegram-bot.py
@@ -275,7 +275,7 @@ def start(bot, update):
                 keyboard = [KeyboardButton(text="/contact")]
 
         if isadmin:
-            keyboard += admin_keyboard
+            keyboard.append(admin_keyboard)
 
         if message and keyboard:
             bot.send_message(chat_id=update.message.chat_id, text=message, parse_mode='Markdown',
