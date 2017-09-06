0d9739050eb37fc48b04d8ec4bb4ce1e4209a18e
strky <shroomshroom@gmail.com>
2017-09-06 19:04:04 +1000

    First commit of working bitcoinj daemon and mercury trading bot

--- mercury-telegram-bot.py
+++ mercury-telegram-bot.py
@@ -581,7 +581,8 @@ def unapproved_actions(bot, update):
 
     if message == "":
         message = "All actions were approved\n"
-        reply_markup = InlineKeyboardMarkup(inline_keyboard=admin_keyboard)
+        # reply_markup = InlineKeyboardMarkup(inline_keyboard=admin_keyboard)
+        reply_markup = ReplyKeyboardRemove()
     else:
         message += "Type *a[n]* to approve\n"
         reply_markup = ReplyKeyboardRemove()
