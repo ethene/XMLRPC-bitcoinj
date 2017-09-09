7b577e5b1656d9f09beefc2737280299822f78fe
strky <shroomshroom@gmail.com>
2017-09-10 01:15:58 +1000

    First commit of working bitcoinj daemon and mercury trading bot

--- mercury-telegram-bot.py
+++ mercury-telegram-bot.py
@@ -4,6 +4,7 @@
 # TODO: imports
 import calendar
 import logging
+import math
 import re
 import shutil
 import traceback
@@ -964,7 +965,10 @@ def health_check(bot, update):
         max_unh_timestamp = calendar.timegm(max_unh_timestamp.utctimetuple()) * 1000
 
         message += "_time is_ *%s* _UTC_\n" % (datetime.utcnow().strftime("%H:%M:%S"))
-        message += "_last unhedge %d m ago_\n" % ((getUTCtime() - max_unh_timestamp) / 60000)
+        last_unh_s = (getUTCtime() - max_unh_timestamp) / 1000
+        last_unh_m = last_unh_s / 60
+        last_unh_h = last_unh_m / 60
+        message += "_last unhedge %d h %d m ago_\n" % (math.floor(last_unh_h), math.floor(last_unh_m))
         message += "_position updated %d s ago_\n" % ((getUTCtime() - max_pos_timestamp) / 1000)
     message += "_updated %d s ago_\n" % ((getUTCtime() - health_record[0]['index']) / 1000)
 
