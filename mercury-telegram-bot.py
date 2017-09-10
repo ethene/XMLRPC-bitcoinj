3c3090a01f639497a9f2e01ef839c36c1076efef
strky <shroomshroom@gmail.com>
2017-09-10 12:27:31 +1000

    First commit of working bitcoinj daemon and mercury trading bot

--- mercury-telegram-bot.py
+++ mercury-telegram-bot.py
@@ -47,11 +47,6 @@ def error_callback(bot, update, error):
         logger.error(e)
         logger.error(traceback.format_exc())
 
-
-hdr = {
-    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
-    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8', 'Pragma': 'no-cache'}
-
 XMLRPCServer = xmlrpc.client.ServerProxy('http://localhost:8000')
 
 block_explorer = 'https://www.blocktrail.com/tBTC/tx/'
