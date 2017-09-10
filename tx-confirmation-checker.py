f109e1f218dc4edb116aafd9bc9c72c5f965ae0f
strky <shroomshroom@gmail.com>
2017-09-11 00:39:12 +1000

    First commit of working bitcoinj daemon and mercury trading bot

--- tx-confirmation-checker.py
+++ tx-confirmation-checker.py
@@ -14,6 +14,7 @@ import telebot
 from settings import MYSQL_CONNECTION, TELEGRAM_BOT_TOKEN
 from sqlalchemy import (create_engine, Table, MetaData)
 from sqlalchemy.sql import select
+from time import sleep
 
 # from telegram import InlineKeyboardButton, \
 #    InlineKeyboardMarkup
@@ -50,27 +51,31 @@ btn = telebot.types.InlineKeyboardButton(text="%s" % emoji.emojize(":arrow_up_sm
 
 XMLRPCServer = xmlrpc.client.ServerProxy('http://localhost:8000')
 
+CYCLE_WAIT = 60
+
 if __name__ == "__main__":
     bot = telebot.TeleBot(TELEGRAM_BOT_TOKEN)
-    with db_engine.connect() as con:
-        j = bitcoinj_transactions.join(useraccounts)
-        q = select([bitcoinj_transactions, useraccounts]).where(bitcoinj_transactions.c.confirmed == False).select_from(
-            j)
-        txs = con.execute(q).fetchall()
-        for t in txs:
-            user_id = t.userID
-            tx_id = t.TXID
-            value = t.value
-            confirmed = XMLRPCServer.isTXconfirmed(tx_id)
-            logger.debug(t)
-            logger.debug(confirmed)
-            if confirmed:
-                upd = bitcoinj_transactions.update().values(confirmed=True).where(
-                    bitcoinj_transactions.c.TXID == tx_id)
-                con.execute(upd)
-                keyboard = telebot.types.InlineKeyboardMarkup()
-                keyboard.add(btn)
-                bot.send_message(chat_id=user_id,
-                                 text="Transaction for\n*%.8f* _BTC_\nis now confirmed\n" % (value / XBt_TO_XBT),
-                                 parse_mode='Markdown',
-                                 reply_markup=keyboard)
+    while (True):
+        with db_engine.connect() as con:
+            j = bitcoinj_transactions.join(useraccounts)
+            q = select([bitcoinj_transactions, useraccounts]).where(
+                bitcoinj_transactions.c.confirmed == False).select_from(
+                j)
+            txs = con.execute(q).fetchall()
+            for t in txs:
+                user_id = t.userID
+                tx_id = t.TXID
+                value = t.value
+                confirmed = XMLRPCServer.isTXconfirmed(tx_id)
+                logger.debug("tx id: %s, user_id: %s, confirmed: %s" % (tx_id, user_id, confirmed))
+                if confirmed:
+                    upd = bitcoinj_transactions.update().values(confirmed=True).where(
+                        bitcoinj_transactions.c.TXID == tx_id)
+                    con.execute(upd)
+                    keyboard = telebot.types.InlineKeyboardMarkup()
+                    keyboard.add(btn)
+                    bot.send_message(chat_id=user_id,
+                                     text="Transaction for\n*%.8f* _BTC_\nis now confirmed\n" % (value / XBt_TO_XBT),
+                                     parse_mode='Markdown',
+                                     reply_markup=keyboard)
+        sleep(CYCLE_WAIT)
