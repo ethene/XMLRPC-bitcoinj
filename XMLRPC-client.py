5e1d83c1ff403fab2c09eeeb8a1d021953221d78
strky <shroomshroom@gmail.com>
2017-10-04 23:33:08 +1100

    First commit of working bitcoinj daemon and mercury trading bot

--- XMLRPC-client.py
+++ XMLRPC-client.py
@@ -8,6 +8,7 @@ s = xmlrpc.client.ServerProxy('http://localhost:8000')
 
 print("TX: %s " % (s.getUnconfirmedTransactions('myt8kNqVm6p8s1F9fp3e4vYQuLCZ8cw3mT')))
 
+print(s.getInputValue('ms4g4oBvhSM31kFrPNfXse8E4uRG7Xz2x4'))
 print("Input value: %s" % s.getInputValue('ms4g4oBvhSM31kFrPNfXse8E4uRG7Xz2x4'))
 
 # sr = s.sendCoins('myt8kNqVm6p8s1F9fp3e4vYQuLCZ8cw3mT', 'mwCwTceJvYV27KXBc3NJZys6CjsgsoeHmf', 10000000)
